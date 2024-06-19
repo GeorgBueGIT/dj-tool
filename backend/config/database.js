a233f8a6b00bfd1b59fbe841f01c886d8bfcc990
geobue852 <02buge1bif@hft-stuttgart.de>
2024-06-19 11:51:15 +0200

    Adding CI with githubactions

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,10 +2,10 @@ import mysql from "mysql2";
 import config from './config.js';
 
 var con = mysql.createConnection({
-  host: config.DB_HOST,
-  user: config.DB_USER,
-  password: config.DB_PASSWORD,
-  database: config.DB_NAME,
+  host: process.env.DB_HOST || config.DB_HOST,
+  user: process.env.DB_USER || config.DB_USER,
+  password: process.env.DB_PASSWORD || config.DB_PASSWORD,
+  database: process.env.DB_NAME || config.DB_NAME,
 });
 
 con.connect(function (err) {
